657cbc19bfa49565011b0c04f26e3d9fa9c0ee24
JDaniel <jdanielxd.3@gmail.com>
2022-05-22 15:34:02 -0700

    Update From VSC

--- package/functions/meme.js
+++ package/functions/meme.js
@@ -2,7 +2,7 @@ const config = require('../config.js');
 const packageName = config.packageName
 
 function meme(){
-    const images = require('../images/memes.js')
+    const images = require('../images/memes.js.js')
     return images[Math.floor(Math.random() * images.length)]
 }
 
