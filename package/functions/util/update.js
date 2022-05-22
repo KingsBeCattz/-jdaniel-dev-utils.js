af291d73919394b3bc4fa7a4df47142c2127b978
JDaniel <jdanielxd.3@gmail.com>
2022-05-22 15:38:40 -0700

    Update From VSC

--- package/functions/util/update.js
+++ package/functions/util/update.js
@@ -5,7 +5,18 @@ function update(){
     const { execSync } = require('child_process')
     const res = execSync(`npm i @jdaniel-dev/utils.js@latest`).toString()
     console.log(res)
-    require('./reboot.js')()
+    try {
+        process.on("exit", () => {
+            require("child_process").spawn(process.argv.shift(), process.argv, {
+                cwd: process.cwd(),
+                detached: true,
+                stdio: "inherit",
+            });
+        });
+        process.exit();
+    } catch (e) {
+        throw new Error(`[${packageName} Error] No podimos reiniciar! Error: ${e.message}`)
+    }
 }
 
 module.exports = update
